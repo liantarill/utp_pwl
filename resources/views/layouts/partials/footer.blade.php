8507de88f89697ce7318bbb85c3a10260e359b11
citrafardiani-24 <citrafardiani@gmail.com>
2025-11-09 22:12:05 +0700

    benerin dikit

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="w-full bg-gray-100 border-t border-gray-300 mt-10">
     <div class="max-w-7xl mx-auto px-6 py-4 text-center">
         <p class="text-gray-600 text-sm">
-            © {{ date('Y') }} Klinik App — All Rights Reserved.
+            © {{ date('Y') }} RS UNILA — All Rights Reserved.
         </p>
     </div>
 </footer>
